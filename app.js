fcb1690b5cb890df6bb31c9be4c3c6cf95dfa169
pazliddinovulugbek <pazliddinovulugbek439@gmail.com>
2024-09-19 22:34:46 +0500

    full page

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const languageSelect = document.getElementById('language-select');
   const defaultLanguage = 'UZB';
 
-  // Load translations from JSON file
   fetch('translations.json')
     .then(response => response.json())
     .then(translations => {
       function setLanguage(language) {
-        // Update text content for elements with IDs
         document.querySelectorAll('[id]').forEach(element => {
           const key = element.id;
           if (translations[language][key]) {
@@ -16,27 +14,21 @@ document.addEventListener('DOMContentLoaded', () => {
         });
       }
 
-      // Set default language
       setLanguage(defaultLanguage);
 
-      // Update language when selected from dropdown
       languageSelect.addEventListener('change', (event) => {
         setLanguage(event.target.value);
       });
     })
     .catch(error => console.error('Error loading translations:', error));
 });
-// app.js
-// app.js
-
-// app.js
 
 document.addEventListener('DOMContentLoaded', function() {
-  var menuIcon = document.querySelector('.menu'); // Menyu ikonkasi
-  var sidebar = document.querySelector('.sidebar'); // Sidebar
+  var menuIcon = document.querySelector('.menu'); 
+  var sidebar = document.querySelector('.sidebar');
 
   menuIcon.addEventListener('click', function() {
-    sidebar.classList.toggle('open'); // Sidebarni ochish/yopish
+    sidebar.classList.toggle('open'); 
   });
 });
 
